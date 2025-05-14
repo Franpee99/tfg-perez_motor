bb8bd5df5d9f45c22807222b901fa7df49f7ea68
Fran <franjoseperezlozano@gmail.com>
2025-05-14 18:44:17 +0200

    Boton de ver las Solicitued de devoluciones añadido al athenticatedlayout

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -155,6 +155,9 @@ export default function AuthenticatedLayout({ header, children }) {
                                             {user?.can?.create_productos && (
                                                 <Link href={route('productos.create')} onClick={() => setMenuAbierto(false)} className="hover:text-red-500 border-b border-white/10 pb-2">Crear producto</Link>
                                             )}
+                                            {user?.can?.viewAny_devoluciones && (
+                                                <Link href={route('admin.devoluciones.index')} onClick={() => setMenuAbierto(false)} className="hover:text-red-500 border-b border-white/10 pb-2">Solicitudes de devolución</Link>
+                                            )}
                                             <Link href={route('pedidos.index')} onClick={() => setMenuAbierto(false)} className="hover:text-red-500 border-b border-white/10 pb-2">Mis pedidos</Link>
                                             <Link href={route('profile.edit')} onClick={() => setMenuAbierto(false)} className="hover:text-red-500 border-b border-white/10 pb-2">Perfil</Link>
                                             <Link href={route('logout')} method="post" as="button" onClick={() => setMenuAbierto(false)} className="hover:text-red-500">Cerrar sesión</Link>
